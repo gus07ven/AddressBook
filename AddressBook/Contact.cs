352741780d378d4dc9000bbfacd86a5962666492
gus07ven <gus07ven@gmail.com>
2016-01-21 16:59:23 -0600

    Upload initial code.

---
+++ AddressBook/Contact.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class Contact
+{
+    String name;                // First name.
+    int phoneNumber;            // Phone number.
+    private string phone;
+
+    public Contact()
+	{
+	}
+
+    public Contact(String a, int b)
+    {
+        this.name = a;
+        this.phoneNumber = b;
+    }
+
+    public Contact(string name, string phone)
+    {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public long getPhoneNumber()
+    {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber)
+    {
+        this.phoneNumber = phoneNumber;
+    }
+
+}
---
+++ AddressBook/ManageContacts.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    class ManageContacts
+    {
+        List<Contact> list = new List<Contact>();
+
+        static void Main(string[] args)
+        {
+
+            // Ask for user to select an option.
+            Console.WriteLine("Please select an option:");
+            Console.WriteLine("1. Add contact");
+            Console.WriteLine("2. List contacts");
+            Console.WriteLine("3. Exit");
+            String input = Console.ReadLine();
+
+            // Loop while user does not select exit.
+            do
+            {
+                switch (input)
+                {
+                    case "1":
+                        AddContact();
+                        break;
+                    case "2":
+                        ListContacts();
+                        break;
+                    default:
+                        
+                        break;
+                }
+            } while (input != "3");
+        }
+
+
+        static String AddContact()
+        {
+            Console.WriteLine("Please enter the name:");
+            String name = Console.ReadLine();
+            Console.WriteLine("Please enter the phone number:");
+            String phone = Console.ReadLine();
+            Contact addPerson = new Contact(name, phone);
+            list.Add(addPerson);
+            String message = "The name and phone number have been added";
+            return message;
+        }
+
+        static Contact ListContacts()
+        {
+            foreach (Contact value in list)
+                Console.WriteLine(value);
+            return null;
+        }
+
+    }
+
+}
