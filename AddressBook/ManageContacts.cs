c1369303abc69ebbc718bcfae191c237903f550e
gus07ven <gus07ven@gmail.com>
2016-02-03 16:39:59 -0600

    Add update and delete contact.

--- AddressBook/ManageContacts.cs
+++ AddressBook/ManageContacts.cs
@@ -30,19 +30,70 @@ namespace AddressBook
                         break;
                     case 2:
                         for (int i = 0; i < list.Count; i++)
-                            Console.WriteLine("{0}, {1}", list[i].name, list[i].phoneNumber);
+                            Console.WriteLine("{0}. {1}, {2}", i + 1, list[i].name, list[i].phoneNumber);
                         Console.WriteLine();
                         break;
                     case 3:
-                        Console.ReadLine();
+                        // Show all contacts in list.
+                        for (int i = 0; i < list.Count; i++)
+                            Console.WriteLine("{0}. {1}, {2} ", i + 1, list[i].name, list[i].phoneNumber);
+
+                        // Select the contact to update.
+                        Console.WriteLine("Please select the number of the contact you would like to update.");
+                        string contactSel = Console.ReadLine();
+                        int choice = Int32.Parse(contactSel) - 1;
+
+                        // Check if contact exists.
+                        if ((list.ElementAtOrDefault(choice) != null))
+
+                            // If contact exist pull contact and ask which field to update.
+                            Console.WriteLine("{0}. {1}, {2} ", choice + 1, list[choice].name, list[choice].phoneNumber);
+                        Console.WriteLine("To update name press 1, to update phone number press 2.");
+                        string selection2 = Console.ReadLine();
+                        int choice2 = Int32.Parse(selection2);
+
+                        // Update new name.
+                        if (choice2 == 1)
+                        {
+                            Console.WriteLine("Please enter the new name:");
+                            list[choice].name = Console.ReadLine();
+                            Console.WriteLine("The name {0} has been updated.", list[choice].name);
+                            Console.WriteLine("{0}. {1}, {2} ", choice + 1, list[choice].name, list[choice].phoneNumber);
+
+                        }
+                        // Update new phone number.
+                        else {
+                            Console.WriteLine("Please enter the new phone number:");
+                            string newPhone = Console.ReadLine();
+                            long newPhone2 = long.Parse(newPhone);
+                            list[choice].phoneNumber = newPhone2;
+                            Console.WriteLine("The new phone number {0} has been updated.", list[choice].phoneNumber);
+                            Console.WriteLine("{0}. {1}, {2} ", choice + 1, list[choice].name, list[choice].phoneNumber);
+
+                        }
+                        break;
+                    case 4:
+                        // Show all contacts in list.
+                        for (int i = 0; i < list.Count; i++)
+                            Console.WriteLine("{0}. {1}, {2} ", i + 1, list[i].name, list[i].phoneNumber);
+
+                        // Select the contact to update.
+                        Console.WriteLine("Please select the number of the contact you would like to delete.");
+                        string contactSel2 = Console.ReadLine();
+                        int choice3 = Int32.Parse(contactSel2) - 1;
+
+                        // Delete contact.
+                        list.Remove(list[choice3]);
+                        Console.WriteLine("The contacts in the list are:");
+                        for (int i = 0; i < list.Count; i++)
+                            Console.WriteLine("{0}. {1}, {2} ", i + 1, list[i].name, list[i].phoneNumber);
+                        Console.WriteLine();
                         break;
                     default:
-                        Console.WriteLine();
-                        menu();
                         break;
                 }
                 selection = menu();
-            } while (selection != 3);        
+            } while (selection != 5);
         }
 
         public static int menu()
@@ -50,7 +101,9 @@ namespace AddressBook
             Console.WriteLine("Please select an option:");
             Console.WriteLine("1. Add a contact.");
             Console.WriteLine("2. Show contact list.");
-            Console.WriteLine("3. Exit.");
+            Console.WriteLine("3. Update contact information.");
+            Console.WriteLine("4. Delete contact.");
+            Console.WriteLine("5. Exit.");
             string enter = Console.ReadLine();
             int input = Int32.Parse(enter);
             return input;
--- AddressBook/Testing.cs
+++ AddressBook/Testing.cs
@@ -8,16 +8,68 @@ namespace AddressBook
 {
     class Testing
     {
-        /*static void Main()
+      /* static void Main()
         {
             List<Contact> list2 = new List<Contact>();
             list2.Add(new Contact("Mike", 1234));
             list2.Add(new Contact("Ron", 56787));
+
+            // Show all contacts in list.
+            for (int i = 0; i < list2.Count; i++)
+                Console.WriteLine("{0}. {1}, {2} ", i + 1, list2[i].name, list2[i].phoneNumber);
+
+            // Select the contact to update.
+            Console.WriteLine("Please select the number of the contact you would like to delete.");
+            string contactSel = Console.ReadLine();
+            int choice = Int32.Parse(contactSel) - 1;
+
+            // Delete contact.
+            list2.Remove(list2[choice]);
+            Console.WriteLine("The contacts in the list are:");
             for(int i = 0; i < list2.Count; i++)
-                Console.WriteLine("{0}, {1}",list2[i].Name, list2[i].PhoneNumber);
-            Console.WriteLine("Press enter to close...");
-            Console.ReadLine();
-        }
-        */
+                Console.WriteLine("{0}. {1}, {2} ", i + 1, list2[i].name, list2[i].phoneNumber);
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+            /*
+            // Show all contacts in list.
+            for (int i = 0; i < list2.Count; i++)
+                Console.WriteLine("{0}. {1}, {2} ", i + 1, list2[i].name, list2[i].phoneNumber);
+
+            // Select the contact to update.
+            Console.WriteLine("Please select the number of the contact you would like to update.");
+            string contactSel = Console.ReadLine();
+            int choice = Int32.Parse(contactSel) - 1;
+
+            // Check if contact exists.
+            if ((list2.ElementAtOrDefault(choice) != null))
+
+                // If contact exist pull contact and ask which field to update.
+                Console.WriteLine("{0}. {1}, {2} ", choice + 1, list2[choice].name, list2[choice].phoneNumber);
+                Console.WriteLine("To update name press 1, to update phone number press 2.");
+                string selection2 = Console.ReadLine();
+                int choice2 = Int32.Parse(selection2);
+
+                // Update new name.
+                    if (choice2 == 1)
+                    {
+                Console.WriteLine("Please enter the new name:");
+                list2[choice].name = Console.ReadLine();
+                Console.WriteLine("The name {0} has been updated.", list2[choice].name);
+                Console.WriteLine("{0}. {1}, {2} ", choice + 1, list2[choice].name, list2[choice].phoneNumber);
+                Console.ReadKey();
+            }
+                // Update new phone number.
+            else {
+                Console.WriteLine("Please enter the new phone number:");
+                string newPhone = Console.ReadLine();
+                long newPhone2 = long.Parse(newPhone);
+                list2[choice].phoneNumber = newPhone2;
+                Console.WriteLine("The new phone number {0} has been updated.", list2[choice].phoneNumber);
+                Console.WriteLine("{0}. {1}, {2} ", choice + 1, list2[choice].name, list2[choice].phoneNumber);
+                Console.ReadKey();
+            }
+           }  
+           */                    
     }
 }
